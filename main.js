30d58b40c7774ff3b2ab8feaad20264774ea806c
Chris McCandlish <cmccand@gmail.com>
2015-04-06 17:49:51 -0400

    getting animations to work

--- main.js
+++ main.js
@@ -11,24 +11,7 @@ $(document).ready(function(){
     $("#screeny").slick('slickPrev');
   });
 
-  (function() {
-    var config = {
-      kitId: 'rij7fwm'
-    };
-    var d = false;
-    var tk = document.createElement('script');
-    tk.src = '//use.typekit.net/' + config.kitId + '.js';
-    tk.type = 'text/javascript';
-    tk.async = 'true';
-    tk.onload = tk.onreadystatechange = function() {
-      var rs = this.readyState;
-      if (d || rs && rs != 'complete' && rs != 'loaded') return;
-      d = true;
-      try { Typekit.load(config); } catch (e) {}
-    };
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(tk, s);
-  })();
+
 
 
 });
