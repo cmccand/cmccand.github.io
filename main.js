abae2589af555d62ad9a70bf48c7c4a18d025945
Chris McCandlish <cmccand@gmail.com>
2015-04-06 17:38:05 -0400

    trying asynchronous typekit load

--- main.js
+++ main.js
@@ -11,4 +11,25 @@ $(document).ready(function(){
     $("#screeny").slick('slickPrev');
   });
 
+  <script type="text/javascript">
+  (function() {
+    var config = {
+      kitId: 'rij7fwm'
+    };
+    var d = false;
+    var tk = document.createElement('script');
+    tk.src = '//use.typekit.net/' + config.kitId + '.js';
+    tk.type = 'text/javascript';
+    tk.async = 'true';
+    tk.onload = tk.onreadystatechange = function() {
+      var rs = this.readyState;
+      if (d || rs && rs != 'complete' && rs != 'loaded') return;
+      d = true;
+      try { Typekit.load(config); } catch (e) {}
+    };
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(tk, s);
+  })();
+  </script>
+
 });
