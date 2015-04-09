dadbe5ec753b55835069b010cf55c1bdc1701dfb
Chris McCandlish <cmccand@gmail.com>
2015-04-09 14:19:06 -0400

    added description and link on hover using jquery because triangles overlay interferes with image behind it

--- main.js
+++ main.js
@@ -11,6 +11,21 @@ $(document).ready(function(){
     $("#screeny").slick('slickPrev');
   });
 
+  $(".img").mouseover(function(){
+    $(".slide").children('.description').css("display", "block");
+    $(".slide").children('.description').css("position", "absolute");
+    $(".slide").children('img').css("position", "relative");
+    $(".slide").children('img').css("opacity", ".2");
+    $(".slide").children('video').css("opacity", ".2");
+  });
+  $(".img").mouseout(function(){
+    $(".slide").children('.description').css("display", "none");
+    $(".slide").children('img').css("opacity", "1");
+    $(".slide").children('video').css("opacity", "1");
+    $(".slide").children('img').css("display", "block");
+    $(".slide").children('video').css("display", "block");
+  });
+
 
 
 
