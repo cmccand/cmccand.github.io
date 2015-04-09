3375344e82fc4736559b1ca49654aa98381049a5
Chris McCandlish <cmccand@gmail.com>
2015-04-09 17:31:04 -0400

    fixed hover on images

--- main.js
+++ main.js
@@ -11,21 +11,21 @@ $(document).ready(function(){
     $("#screeny").slick('slickPrev');
   });
 
-  $(".img").mouseover(function(){
-    $(".slide").children('.description').css("display", "block");
-    $(".slide").children('.description').css("position", "absolute");
-    $(".slide").children('.link').css("display","block");
-    $(".slide").children('img').css("position", "relative");
-    $(".slide").children('img').css("opacity", ".2");
-    $(".slide").children('video').css("opacity", ".2");
-  });
-  $(".img").mouseout(function(){
-    $(".slide").children('.description').css("display", "none");
-    $(".slide").children('img').css("opacity", "1");
-    $(".slide").children('video').css("opacity", "1");
-    $(".slide").children('img').css("display", "block");
-    $(".slide").children('video').css("display", "block");
-  });
+  // $(".img").mouseover(function(){
+  //   $(".slide").children('.description').css("display", "block");
+  //   $(".slide").children('.description').css("position", "absolute");
+  //   $(".slide").children('.link').css("display","block");
+  //   $(".slide").children('img').css("position", "relative");
+  //   $(".slide").children('img').css("opacity", ".2");
+  //   $(".slide").children('video').css("opacity", ".2");
+  // });
+  // $(".img").mouseout(function(){
+  //   $(".slide").children('.description').css("display", "none");
+  //   $(".slide").children('img').css("opacity", "1");
+  //   $(".slide").children('video').css("opacity", "1");
+  //   $(".slide").children('img').css("display", "block");
+  //   $(".slide").children('video').css("display", "block");
+  // });
 
   // $('.link').click(function() {
   //   window.location='http://cmccand.github.io/Group-2/client/';
