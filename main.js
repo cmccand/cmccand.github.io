5963674262a516b02ed4e6c68b0f59128f3a87bb
Chris McCandlish <cmccand@gmail.com>
2015-04-12 15:59:42 -0400

    added About Me modal

--- main.js
+++ main.js
@@ -15,6 +15,25 @@ $(document).ready(function(){
     $("#screeny").slick('slickNext');
   });
 
+  $(function() {
+    $("#modal-1").on("change", function() {
+      if ($(this).is(":checked")) {
+        $("body").addClass("modal-open");
+      } else {
+        $("body").removeClass("modal-open");
+      }
+    });
+
+    $(".modal-fade-screen, .modal-close").on("click", function() {
+      $(".modal-state:checked").prop("checked", false).change();
+    });
+
+    $(".modal-inner").on("click", function(e) {
+      e.stopPropagation();
+    });
+  });
+  
+
 
   // $(".img").mouseover(function(){
   //   $(".slide").children('.description').css("display", "block");
