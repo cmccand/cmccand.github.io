37ce05947ae4c299ef06ff0d5ebf2b03e8a2903a
Chris McCandlish <cmccand@gmail.com>
2015-04-12 19:33:03 -0400

    testing styles

--- main.js
+++ main.js
@@ -32,7 +32,25 @@ $(document).ready(function(){
       e.stopPropagation();
     });
   });
-  
+
+  $(function() {
+    $("#modal-2").on("change", function() {
+      if ($(this).is(":checked")) {
+        $("body").addClass("modal-open");
+      } else {
+        $("body").removeClass("modal-open");
+      }
+    });
+
+    $(".modal-fade-screen, .modal-close").on("click", function() {
+      $(".modal-state:checked").prop("checked", false).change();
+    });
+
+    $(".modal-inner").on("click", function(e) {
+      e.stopPropagation();
+    });
+  });
+
 
 
   // $(".img").mouseover(function(){
